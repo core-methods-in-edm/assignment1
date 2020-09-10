1e180db1403b1c7da261c54be97dfc0d26edffb9
Cypress2019 <aprilchang1023@gmail.com>
2020-09-10 18:30:04 -0400

    initial save

---
+++ assignment1.R
@@ -0,0 +1,9 @@
+# Assignment 1
+https://cypresspic.wixsite.com/cypresspic
+
+## My Goals for HUDK4050
+*gain R proficiency
+*further develop data-analytical thinking
+*get to know at least one classmate
+
+##Video Review
