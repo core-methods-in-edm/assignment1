0071622b7942f7c2d48944ac98906fc5e0eda227
Cypress2019 <62275164+Cypress2019@users.noreply.github.com>
2020-09-16 13:21:14 -0400

    Delete assignment1.R

--- assignment1.R
+++
@@ -1,9 +0,0 @@
-# Assignment 1
-https://cypresspic.wixsite.com/cypresspic
-
-## My Goals for HUDK4050
-*gain R proficiency
-*further develop data-analytical thinking
-*get to know at least one classmate
-
-##Video Review
